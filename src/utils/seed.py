a23c9bbba5196b3242a5b20d198cf9ce15f77624
bsj2061 <bsj2061@gmail.com>
2026-02-04 13:14:47 +0900

    feat seed

---
+++ src/utils/seed.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import random
+
+def set_seed(seed: int = 42):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
