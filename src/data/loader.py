4695479451808cf1298ed5a767e787383b71ccab
bsj2061 <bsj2061@gmail.com>
2026-02-04 14:42:26 +0900

    rename from dataloader.py to loader.py

---
+++ src/data/loader.py
@@ -0,0 +1,12 @@
+from torch.utils.data import DataLoader
+
+def build_loader(dataset, batch_size, shuffle):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=4,
+        pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=4
+    )
